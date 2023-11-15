9b164264aa451842fd7c657a246442c5563650ca
Tongwa Aka <tongwa.aka@invitahealth.com>
2023-11-15 16:46:37 -0500

    Saving local phpservermon

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+define('PSM_DB_PREFIX', 'monitor_');
+define('PSM_DB_USER', 'cmtuser');
+define('PSM_DB_PASS', 'Cmt01');
+define('PSM_DB_NAME', 'phpservermon');
+define('PSM_DB_HOST', 'localhost');
+define('PSM_DB_PORT', '3306');
+define('PSM_BASE_URL', 'http://localhost/phpservermon-3.2.0');
